d9e1ee65e60023535121456116d148749f2f76bb
04schaudhari <mandesayali04@gmail.com>
2023-12-26 16:41:41 +0530

    01_Variable Assignment
    Declaration and Initialized variable assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,30 @@
+var myFullName;
+myFullName="Sayali Chaudhari";
+console.log("Name:", myFullName);
+
+myFullName="Sayali Sagar Chaudhari";
+console.log("Full Name:", myFullName);
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+var myLove;
+myLove="I Love Only JS"
+console.log("MyLove:",myLove);
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+var firstName="Sayali";
+console.log("First Name:",firstName);
+
+var lastName="Chaudhari";
+console.log("Last Name:", lastName);
+
+var age="30";
+console.log("Age:", age);
+
+var address="New Sangavi Pune"
+console.log("Address:",address);
+
+var pincode = "411061";
+console.log("Pincode : ", pincode);
+var gender = "Female";
+console.log("Gender : ", gender);
+var isMarried = "Yes";
+console.log("Is Married : ", isMarried);
---
+++ helloWord.js
@@ -0,0 +1,41 @@
+console.log("Hello JS, I am Sayali");
+var num;
+console.log(num);
+
+var num="10";
+console.log(num);
+
+var myName="Sayali";
+console.log(myName);
+
+console.log("First Name",myName);
+
+myName="Sayali Chaudhari";
+console.log("First Name",myName);
+
+
+console.log("Hello JS , I am Gajanan");
+
+ 
+
+var myName; // Varibale Declartion
+
+console.log(myName); // undefined
+
+ 
+
+myName = "Sayali"; // Variable Intialization
+
+console.log("First Name: ",myName);
+
+ 
+
+var age = "32"; // Varibale declaration and Initialization
+
+console.log("Age:", age);
+
+ 
+
+myName = "Sayali Chaudhari"; // Varibale update
+
+console.log("Full name: ", myName);
