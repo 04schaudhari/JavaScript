d1ea6c6b34f449c645c2edd9781af12422230c9d
04schaudhari <mandesayali04@gmail.com>
2024-01-02 17:51:31 +0530

    update 05_operatorA
    function string

--- 05_operatorA.js
+++ 05_operatorA.js
@@ -17,16 +17,19 @@ var returnResult = squareOfWordLenght("Developer Smart");
 console.log(`Length of square is: ${returnResult}\n`);
 console.log( `-----------------------step2-------------------------------`);
 
-function string() {
-    var string = "I am React Developer";
-    console.log(`Given string is: ${string}`);
-    var result = string.length;
-    console.log(`Length of string is: ${result}`);
-    var word = string.split(" ");
-    console.log(`Total number of words are: ${word.length}\n`);
-    var div = result / word.length;
-    console.log(`Division of total words: ${div}`);
-    var mul = result * word.length;
-    console.log(`Multiplication of total words: ${mul}`);
-  }
-  string();
+function noValue() {
+    var string = "I am Angular Developer";
+    console.log(`The Given String is ${string}`);
+    var Stringlen = string.length;
+    console.log(`The Length of Given String is ${Stringlen}`);
+
+    var wordsavailabel= string.split(" ");
+    console.log(`The words in Given String is ${wordsavailabel}`);
+
+ var divideby = Stringlen / wordsavailabel.length;
+ console.log(`After dividing  ${divideby}`);
+
+  var multiplication = Stringlen* wordsavailabel.length;
+  console.log(`Total words available in string after multiplication is ${multiplication}`);
+}
+noValue();
