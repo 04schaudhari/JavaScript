d0077dc72b1c09f91f85607b399454e0029e594d
04schaudhari <mandesayali04@gmail.com>
2024-03-19 17:27:26 +0530

    19-3-2024
    Changes on 2nd program

--- CODEMIND Logic program/last character.js
+++ CODEMIND Logic program/last character.js
@@ -7,8 +7,10 @@ function givenString(str1) {
     console.log(`Given String length are: ${result}`);
 
     var lastChar = str1.charAt(str1.length-1)
-    console.log(`last char is: ${lastChar}`);
+    console.log(`last character of the given strings is: ${lastChar}\n`);
     
 }
 
 givenString("Hey, my friend, Programming Language")
+givenString("I am learning logical programs")
+givenString("Angular")
